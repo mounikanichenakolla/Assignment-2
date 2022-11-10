8c2d35e776e10bda1dd6c412419247cc8ed4674f
mounikanichenakolla <115568218+mounikanichenakolla@users.noreply.github.com>
2022-11-10 22:31:33 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class MergeSort {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int [] firstArray = {98,56,38,29,54};
 		int [] secondArray = {85,78,34,72,11};
 		int length = firstArray.length + secondArray.length;
@@ -23,5 +24,7 @@ public class MergeSort {
 		Arrays.sort(mergedArray);
 		System.out.println("The MergeSort Array is:"+" " +Arrays.toString(mergedArray));
 	
+	
 	}
+
 }
