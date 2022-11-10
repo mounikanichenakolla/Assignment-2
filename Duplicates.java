4c7a3e13ea42dfff5e8b669782c5c04a73f46b8e
mounikanichenakolla <115568218+mounikanichenakolla@users.noreply.github.com>
2022-11-10 22:26:27 +0530

    Update Duplicates.java

--- Duplicates.java
+++ Duplicates.java
@@ -6,13 +6,14 @@ public class Duplicates {
 
 	public static void main(String[] args) {
 		
-	
+	System.out.println("Duplicate Elements are:");
 		boolean flag1 =false;
 		String []arr = {"Hi", "Hello", "Welcome", "To", "INeuron",  "Hi",  "INeuron"};
+		
 		for(int i=0;i<arr.length;i++) {
 			for(int j=i+1;j<arr.length;j++) {
 				if(arr[i]==arr[j]) {
-					System.out.println("Duplicate Elements are:" + " " +arr[j]);
+					System.out.println(arr[j]+" ");
 					flag1 = true;
 					break;
 				}
